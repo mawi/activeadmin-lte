48cfb717f9f413b8ca614c2f940200f5a1746ed9
stephenaument <saument@gmail.com>
2016-11-19 18:23:16 -0600

    initialize tinymce

--- app/assets/javascripts/admin-lte/app.js
+++ app/assets/javascripts/admin-lte/app.js
@@ -744,3 +744,23 @@ function _init() {
     });
   };
 }(jQuery));
+
+$(document).ready(function() {
+  tinymce.init({
+    selector: 'textarea',
+    theme: 'modern',
+    plugins: [
+      'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+      'searchreplace wordcount visualblocks visualchars code fullscreen',
+      'insertdatetime media nonbreaking save table contextmenu directionality',
+      'emoticons template paste textcolor colorpicker textpattern imagetools codesample'
+    ],
+    toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
+    image_advtab: true,
+    content_css: [
+      '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+      '//www.tinymce.com/css/codepen.min.css'
+    ]
+  });
+});
